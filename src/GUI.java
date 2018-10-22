5efd1b440f8d88e2741e4510f9a61bd7696fa298
Meet Digrajkar <meetdigrajkar@cmail.carleton.ca>
2018-10-21 23:19:49 -0400

    Added One line input for plant location

--- src/GUI.java
+++ src/GUI.java
@@ -4,36 +4,67 @@ import java.util.ArrayList;
 public class GUI {
 	public static final int GRID_HEIGHT = 4;
 	public static final int GRID_WIDTH = 7;
-	public static final String GRID_X[] = {"A","B","C","D","E","F","G"};  //Must be same length as GRID_WIDTH
-	public static final String GRID_Y[] = {"1","2","3","4"};
+	public static final String GRID_X[] = { "A", "B", "C", "D", "E", "F", "G" }; // Must be same length as GRID_WIDTH
+	public static final String GRID_Y[] = { "1", "2", "3", "4" };
 	public static GridObject[][] grid;
 	public static ArrayList<GridObject> gridObjects = new ArrayList<GridObject>();
-	//public static ArrayList<Plant> boardPlant = new ArrayList<Plant>();
-	//public static ArrayList<Zombie> boardZombie = new ArrayList<Zombie>();
-	
+	// public static ArrayList<Plant> boardPlant = new ArrayList<Plant>();
+	// public static ArrayList<Zombie> boardZombie = new ArrayList<Zombie>();
+
 	public GUI() {
 		setupGrid();
 	}
 	
+	/*
+	 * This method returns the index at which the given character (its really a string) is at
+	 * 
+	 * @Param String c, a single valued string
+	 * @Return the index at which the String c was found first.
+	 */
+	public static int getGridX(String c) {
+		for (int i = 0; i < GRID_X.length; i++) {
+			if(c.equals(GRID_X[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/*
+	 * This method returns the index at which the given character (its really a string) is at
+	 * 
+	 * @Param String c, a single valued string
+	 * @Return the index at which the String c was found first.
+	 */
+	public static int getGridY(String c) {
+		for (int i = 0; i < GRID_Y.length; i++) {
+			if(c.equals(GRID_Y[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	public static void setupGrid() {
 		grid = new GridObject[GRID_HEIGHT][GRID_WIDTH];
-		
+
 		for (int i = 0; i < GRID_HEIGHT; i++) {
 			for (int j = 0; j < GRID_WIDTH; j++) {
 				grid[i][j] = new NullSpace();
 			}
-		} printGrid();
+		}
+		printGrid();
 	}
-	
+
 	public static void printGrid() {
 		System.out.print("   ");
-		for (String s: GRID_X) {
+		for (String s : GRID_X) {
 			System.out.print(s + "    ");
 		}
 		System.out.println();
 		for (int i = 0; i < GRID_HEIGHT; i++) {
 			for (int j = 0; j < GRID_WIDTH; j++) {
-				if (j==0)
+				if (j == 0)
 					System.out.print(i + 1);
 				System.out.print(grid[i][j]);
 			}
@@ -41,7 +72,7 @@ public class GUI {
 		}
 		System.out.println();
 	}
-	
+
 	public static void placePlant(Plant plant, int posY, int posX) {
 		grid[posX][posY] = plant;
 		gridObjects.add(plant);
@@ -53,7 +84,7 @@ public class GUI {
 		gridObjects.add(zombie);
 		printGrid();
 	}
-	
+
 //	public int getXPos(GridObject object) {
 //		
 //	}
@@ -61,14 +92,15 @@ public class GUI {
 //	public int getYPos(GridObject object) {
 //	
 //	}
-	
-	public static GridObject toTheLeft(GridObject zombie) { //mostly just called by zombies but no need to specify
+
+	public static GridObject toTheLeft(GridObject zombie) { // mostly just called by zombies but no need to specify
 		int j = getX(zombie);
-		if (j - 1 == 0) //Beginning of board, Plants LOSE! Also returning null is not the same as NullSpace
+		if (j - 1 == 0) // Beginning of board, Plants LOSE! Also returning null is not the same as
+						// NullSpace
 			return null;
 		int i = getY(zombie);
 		try {
-			return (grid[i][j-1]);
+			return (grid[i][j - 1]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Index out of bounds in To the left int i = " + i + "  j = " + j);
 			printGrid();
@@ -76,13 +108,13 @@ public class GUI {
 		}
 		return null;
 	}
-	
+
 	public static GridObject toTheRight(GridObject plant) {
 		int j = getX(plant);
 		int i = getY(plant);
-		//Remember plants cannont be placed in zombie spawn			
+		// Remember plants cannont be placed in zombie spawn
 		try {
-			return (grid[i][j+1]);
+			return (grid[i][j + 1]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Index out of bounds in To the Right int i = " + i + "  j = " + j);
 			printGrid();
@@ -90,9 +122,8 @@ public class GUI {
 		}
 		return null;
 	}
-	
-	
-	public static void move(GridObject gridObject, NullSpace nullSpace){
+
+	public static void move(GridObject gridObject, NullSpace nullSpace) {
 		int j = getX(gridObject);
 		int i = getY(gridObject);
 		int jnext = getX(nullSpace);
@@ -100,17 +131,16 @@ public class GUI {
 		grid[inext][jnext] = gridObject;
 		grid[i][j] = nullSpace;
 	}
-	
-	
+
 	public static void remove(GridObject gridObject) {
 		int j = getX(gridObject);
 		int i = getY(gridObject);
 		grid[i][j] = new NullSpace();
 		gridObjects.remove(gridObject);
 	}
-	
+
 	private static int getX(GridObject gridObject) {
-		int i,j = 0;
+		int i, j = 0;
 		for (i = 0; i < GRID_HEIGHT; i++) {
 			for (j = 0; j < GRID_WIDTH; j++) {
 				if (grid[i][j].equals(gridObject))
@@ -119,9 +149,9 @@ public class GUI {
 		}
 		return -1;
 	}
-	
+
 	private static int getY(GridObject gridObject) {
-		int i,j = 0;
+		int i, j = 0;
 		for (i = 0; i < GRID_HEIGHT; i++) {
 			for (j = 0; j < GRID_WIDTH; j++) {
 				if (grid[i][j].equals(gridObject))
@@ -130,12 +160,12 @@ public class GUI {
 		}
 		return -1;
 	}
-	
+
 	public static boolean isEmpty(int posY, int posX) {
-		return (getObject(posX,posY) instanceof NullSpace);
+		return (getObject(posX, posY) instanceof NullSpace);
 	}
-	
+
 	private static GridObject getObject(int i, int j) {
-		return grid[i][j];	
+		return grid[i][j];
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-	static Scanner reader = new Scanner(System.in); //need to declare here because it must stay open most of the game and player
-	//turn must have access. when a scanner is closed so is it's input stream.
-	
-	private int num;
-	
+	static Scanner reader = new Scanner(System.in); // need to declare here because it must stay open most of the game
+													// and player
+	// turn must have access. when a scanner is closed so is it's input stream.
+
 	public static void main(String[] args) {
 		System.out.println("At any point in the game enter 'h' for game options\n");
 		try {
@@ -18,26 +17,25 @@ public class Main {
 		Level.level1();
 		GUI gui = new GUI();
 
-		
-		
-		while(true) {
+		while (true) {
 			playerTurn();
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			//boardTurn();
-			//System.out.println("Zombies are coming..."); //Next few lines to be replaced with a pause
+			// boardTurn();
+			// System.out.println("Zombies are coming..."); //Next few lines to be replaced
+			// with a pause
 //			try {
 //				Thread.sleep(5000);
 //			} catch (InterruptedException e) {
 //				e.printStackTrace();
 //			}
-			
+
 			System.out.println();
-			//zombieTurn();
-			
+			// zombieTurn();
+
 			boardTurn();
 			try {
 				Thread.sleep(2000);
@@ -47,22 +45,24 @@ public class Main {
 			prepareNewTurn();
 		}
 	}
-	
+
 	public static void boardTurn() {
 		ArrayList<Zombie> zombiesOnBoard = new ArrayList<Zombie>();
-		ArrayList<Plant> plantsOnBoard = new ArrayList<Plant>(); //These two arraylists are defined to avoid concurrent modification exception. Can't iterate through
-		//zombies and plants at the same time in case one dies.
-		for (GridObject gridObject: GUI.gridObjects) {
+		ArrayList<Plant> plantsOnBoard = new ArrayList<Plant>(); // These two arraylists are defined to avoid concurrent
+																	// modification exception. Can't iterate through
+		// zombies and plants at the same time in case one dies.
+		for (GridObject gridObject : GUI.gridObjects) {
 			if (gridObject instanceof Plant)
-				plantsOnBoard.add((Plant)gridObject);
+				plantsOnBoard.add((Plant) gridObject);
 		}
-		
-		for (GridObject gridObject: GUI.gridObjects) {
+
+		for (GridObject gridObject : GUI.gridObjects) {
 			if (gridObject instanceof Zombie)
-				zombiesOnBoard.add((Zombie)gridObject);
-		}  // This is just to check if zombies are empty at start. Array will need to be reconstructed for the zombies turn in case one dies.
-		
-		if (!zombiesOnBoard.isEmpty()) { //This should be checked elsewhere instead of the set up above
+				zombiesOnBoard.add((Zombie) gridObject);
+		} // This is just to check if zombies are empty at start. Array will need to be
+			// reconstructed for the zombies turn in case one dies.
+
+		if (!zombiesOnBoard.isEmpty()) { // This should be checked elsewhere instead of the set up above
 			System.out.println("Time for the attacks");
 			System.out.println();
 			try {
@@ -70,33 +70,32 @@ public class Main {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			for (Plant plant: plantsOnBoard) { //Plants attack first. Need to iterate through twice in case a zombie dies.
+			for (Plant plant : plantsOnBoard) { // Plants attack first. Need to iterate through twice in case a zombie
+												// dies.
 				plant.go();
 			}
 			zombiesOnBoard = new ArrayList<Zombie>();
-			for (GridObject gridObject: GUI.gridObjects) {
+			for (GridObject gridObject : GUI.gridObjects) {
 				if (gridObject instanceof Zombie)
-					zombiesOnBoard.add((Zombie)gridObject);
+					zombiesOnBoard.add((Zombie) gridObject);
 			} // Reconstruct array in case a zombie was killed by a plant
-			for (Zombie zombie: zombiesOnBoard) { //Plants attack first. Need to iterate through twice in case a zombie dies.
+			for (Zombie zombie : zombiesOnBoard) { // Plants attack first. Need to iterate through twice in case a
+													// zombie dies.
 				zombie.go();
 			}
 			GUI.printGrid();
 		}
 		spawnZombies();
-	} 
-	
-	
-	
-	public static void playerTurn() {	
+	}
+
+	public static void playerTurn() {
 		while (true) {
 			Level.printAllPlants();
 			System.out.println();
 			System.out.println("You have " + Level.coins + " coins");
 			System.out.println();
-			
-			
-			if (!(plantAvailable())||!(plantAffordable())) {
+
+			if (!(plantAvailable()) || !(plantAffordable())) {
 				try {
 					Thread.sleep(1500);
 				} catch (InterruptedException e) {
@@ -113,11 +112,9 @@ public class Main {
 				}
 				break;
 			}
-				
-				
-			
+
 			System.out.print("Pick a Plant: ");
-			String pChoice = reader.next(); 
+			String pChoice = reader.next();
 			if (pChoice.equals("h"))
 				printHelp();
 			Plant plantChoice = createPlant(pChoice);
@@ -130,51 +127,40 @@ public class Main {
 					plantChoice = createPlant(pChoice);
 				System.out.println();
 			}
+
+			boolean notAllowedLocation = true;
+			String sxPos = "";
+			String syPos = "";
 			
-			int xPos = -1;
-			while (xPos < 0) {
-				System.out.print("Pick Position X: ");
-				String sxPos = reader.next();
-				if (sxPos.equals("G"))
-					xPos = -2;
-				for (int i = 0; i < GUI.GRID_WIDTH - 1; i++) {
-					if(sxPos.equals(GUI.GRID_X[i])) {
-						xPos = i;
-						break;
-					}
+			//loop checks if the location specified by the player is allowed
+			while (notAllowedLocation) {
+				System.out.print("Pick Position: ");
+				String location = reader.next();
+				System.out.println(location.charAt(0) + "   " + location.charAt(1));
+				if ((location.charAt(0) >= 'A' && location.charAt(0) <= 'G') && ((Character.getNumericValue(location.charAt(1)) >= 1) && (Character.getNumericValue(location.charAt(1)) <= 4) )) {
+					// check if entered column (Letter) is in between A and G
+					// check if entered row (number) is in between 1 and 4
+					sxPos += location.charAt(0);
+					syPos += location.charAt(1);
+					notAllowedLocation = false;
 				}
-				if (xPos == -1)
-					System.out.println("Invalid input must be the capital Alphabet letter corresponding to the X Axis");
-				if (xPos == -2)
-					System.out.println("Sorry, you cannot place your plant in the zombie's spawn");
 			}
 			
-			System.out.print("Pick Position Y: ");
-			int yPos = -1;
-			while (yPos == -1) {
-				String syPos = reader.next();
-				for (int i = 0; i < GUI.GRID_HEIGHT; i++) {
-					if(syPos.equals(GUI.GRID_Y[i])) {
-						yPos = i;
-						break;
-					}
-				}
-				if (yPos == -1)
-					System.out.print("Invalid input must be the number corresponding to the Y Axis.\nTry again: ");
-			}
-			if (GUI.isEmpty(xPos, yPos)) {
-				GUI.placePlant(plantChoice, xPos, yPos);
-				plantChoice.resetTime(); //Count down to use this plant restarts
+			if (GUI.isEmpty(GUI.getGridX(sxPos), GUI.getGridY(syPos))) {
+				GUI.placePlant(plantChoice, GUI.getGridX(sxPos), GUI.getGridY(syPos));
+				plantChoice.resetTime(); // Count down to use this plant restarts
 				break;
 			}
-			//BUG HERE!!!!! Coins are not returned to player and chosen plant's timer has been reset
-			//Solution1 allow player to reset only XY values (easier)
-			//Solution2 somehow create plants only after XY is chosen then allow player to completely reset choice (preferred)
+			// BUG HERE!!!!! Coins are not returned to player and chosen plant's timer has
+			// been reset
+			// Solution1 allow player to reset only XY values (easier)
+			// Solution2 somehow create plants only after XY is chosen then allow player to
+			// completely reset choice (preferred)
 			System.out.println("You may only place your plant on an available space. Please try again.");
 		}
-		
+
 	}
-	
+
 	private static void spawnZombies() {
 		if (Level.allZombies.isEmpty())
 			return;
@@ -191,31 +177,33 @@ public class Main {
 			e.printStackTrace();
 		}
 		int yPos = ThreadLocalRandom.current().nextInt(0, 3);
-		int randZombie = ThreadLocalRandom.current().nextInt(0, Level.allZombies.size()); //zombies are filled in order of type need to pick randomly
-		Zombie zombie = Level.allZombies.remove(randZombie); //Unlike plant this contains all zombies for the level. Must remove to place on board.
-		
-		if (GUI.isEmpty(GUI.GRID_WIDTH-1 , yPos))  //Just doesn't happen if the intended position is occupied. This should be fixed
-			GUI.placeZombie(zombie, GUI.GRID_WIDTH-1 , yPos);
+		int randZombie = ThreadLocalRandom.current().nextInt(0, Level.allZombies.size()); // zombies are filled in order
+																							// of type need to pick
+																							// randomly
+		Zombie zombie = Level.allZombies.remove(randZombie); // Unlike plant this contains all zombies for the level.
+																// Must remove to place on board.
+
+		if (GUI.isEmpty(GUI.GRID_WIDTH - 1, yPos)) // Just doesn't happen if the intended position is occupied. This
+													// should be fixed
+			GUI.placeZombie(zombie, GUI.GRID_WIDTH - 1, yPos);
 	}
-	
+
 	private static Plant createPlant(String choice) {
-		for (Plant plant: Level.allPlants) {
-			if (choice.equals(plant.toShortString())){
+		for (Plant plant : Level.allPlants) {
+			if (choice.equals(plant.toShortString())) {
 				if (!(plant.isAvailable())) {
 					System.out.println("This plant is not available yet");
 					return null;
-				}
-				else if (!(plant.isAffordable())) {
+				} else if (!(plant.isAffordable())) {
 					System.out.println("You cannot afford this plant");
 					return null;
-				}
-				else {
+				} else {
 					if (choice.equals("S"))
 						return new Sunflower();
 					else if (choice.equals("V"))
 						return new VenusFlyTrap();
-					///PUT MORE PLANTS HERE
-					else 
+					/// PUT MORE PLANTS HERE
+					else
 						System.out.println("MUST DEFINE THIS PLANT IN CREATE FUNCTION");
 				}
 			}
@@ -224,36 +212,37 @@ public class Main {
 		return null;
 	}
 
-		
-	
-	//Seems redundant atm but thinking there may be more actions to be performed at the end of board turn later.
+	// Seems redundant atm but thinking there may be more actions to be performed at
+	// the end of board turn later.
 	private static void prepareNewTurn() {
-		for (Plant plant : Level.allPlants) //This function just decreases the wait time for all plant types. Does this to one of each plant
-			plant.newTurn(); 
-		for (GridObject plant : GUI.gridObjects) { //Possibility to extend this to other objects and call a method they all inherit instead of instance of.
+		for (Plant plant : Level.allPlants) // This function just decreases the wait time for all plant types. Does this
+											// to one of each plant
+			plant.newTurn();
+		for (GridObject plant : GUI.gridObjects) { // Possibility to extend this to other objects and call a method they
+													// all inherit instead of instance of.
 			if (plant instanceof Sunflower) {
 				Level.coins += Sunflower.COIN_BONUS;
 			}
-				
+
 		}
 	}
-	
+
 	private static void printHelp() {
 		System.out.println("THIS IS THE HELP TEXT, WILL SHOW YOU PLANT ZOMBIE TYPES ETC");
 		System.out.println();
 	}
 
 	private static boolean plantAvailable() {
-		for (Plant plant: Level.allPlants) {
+		for (Plant plant : Level.allPlants) {
 			if (plant.isAvailable()) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	private static boolean plantAffordable() {
-		for (Plant plant: Level.allPlants) {
+		for (Plant plant : Level.allPlants) {
 			if (plant.isAffordable()) {
 				return true;
 			}
@@ -261,4 +250,3 @@ public class Main {
 		return false;
 	}
 }
-
