a15c8bfb37bd33fa50283fe163993ca12bb661eb
Robyn <robynvg21@gmail.com>
2018-11-13 23:35:48 -0500

    part of gridobject refactoring

--- src/model/NullSpace.java
+++ src/model/NullSpace.java
@@ -1,13 +1,4 @@
 package model;
 
 public class NullSpace extends GridObject {
-
-	/**
-	 * This method prints out a null space string on the grid. 
-	 * @return A String, this method returns a null space string equivalent.
-	 */
-	@Override
-	public String toString() {
-		return "[   ]";
-	}
 }
