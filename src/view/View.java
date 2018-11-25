3032be95cf8f8066e41efc75eaf65002ae45986d
Britney <bakebr75@gmail.com>
2018-11-24 19:57:17 -0500

    bug city

--- src/view/View.java
+++ src/view/View.java
@@ -21,20 +21,20 @@ import model.Zombie;
 public class View extends JFrame {
 	public static final int GRID_HEIGHT = 4;// The Height of the board.
 	public static final int GRID_WIDTH = 7;// The Width of the board.
-	
+
 	// Menu
 	private JMenuBar menuBar;// The menu bar
 	private JMenu menu; // The menu
 	private JMenuItem start, restart, help; // Menu items
-	
-	private JLabel coins; //label to hold coin values
-	
+
+	private JLabel coins; // label to hold coin values
+
 	private JButton undoTurn, endTurn, redoTurn;
-	
+
 	// Buttons(Plant VS Zombies Grid)
 	private JPanel gridLayoutButtons;
 	public static JButton[][] buttons; // A Button Array that retains the Buttons
-	
+
 	// Plant choices
 	private JList<JPanel> menuList;
 
@@ -72,7 +72,11 @@ public class View extends JFrame {
 				buttons[i][j] = new JButton("");
 				buttons[i][j].setEnabled(false);
 				buttons[i][j].setActionCommand(i + " " + j);
-				buttons[i][j].setBackground(new Color(154,205,50));
+				if((i + j) % 2 == 0 ) {
+					buttons[i][j].setBackground(new Color(154,205,50));
+				}else{
+					buttons[i][j].setBackground(new Color(173,255,47));
+				}
 				gridLayoutButtons.add(buttons[i][j]);
 			}
 		}
@@ -87,21 +91,36 @@ public class View extends JFrame {
 				new ImageIcon("resources/SunFlower.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 		ImageIcon venusflytrapImage = new ImageIcon(
 				new ImageIcon("resources/VenusFlyTrap.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
-
+		ImageIcon walnutImage = new ImageIcon(
+				new ImageIcon("resources/Walnut.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+		ImageIcon potatoeImage  = new ImageIcon(
+				new ImageIcon("resource/Potatoe.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+		ImageIcon peaShooterImage = new ImageIcon(
+				new ImageIcon("resource/PeaShooter.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 		// add images to the labels
 		JLabel sunflowerLabel = new JLabel("SunFlower", sunflowerImage, JLabel.LEFT);
 		JLabel venusflytrapLabel = new JLabel("VenusFlyTrap", venusflytrapImage, JLabel.LEFT);
+		JLabel walnutLabel = new JLabel("Walnut", walnutImage, JLabel.LEFT);
+		JLabel potatoeLabel = new JLabel("Potatoe",potatoeImage, JLabel.LEFT);
+		JLabel peaShooterLabel = new JLabel("PeaShooter",peaShooterImage, JLabel.LEFT);
 		
 		// create panels for each plant
 		JPanel sunflowerPanel = new JPanel();
 		JPanel venusflytrapPanel = new JPanel();
+		JPanel walnutPanel = new JPanel();
+		JPanel potatoePanel = new JPanel();
+		JPanel peaShooterPanel = new JPanel();
 
 		// add JLabel to JPanel
 		sunflowerPanel.add(sunflowerLabel);
 		venusflytrapPanel.add(venusflytrapLabel);
+		walnutPanel.add(walnutLabel);
+		potatoePanel.add(potatoeLabel);
+		peaShooterPanel.add(peaShooterLabel);
+		
 
 		// array of panels
-		JPanel[] panels = { sunflowerPanel, venusflytrapPanel };
+		JPanel[] panels = { sunflowerPanel, venusflytrapPanel, walnutPanel, potatoePanel, peaShooterPanel};
 
 		// JList of panels
 		menuList.setListData(panels);
@@ -151,140 +170,138 @@ public class View extends JFrame {
 		//calls the information frame method
 		 makeInfoFrame();
 	}
-	
-	
+
 	/**
 	 * This method is the information frame that displays the plants prices.
 	 */
-	public void makeInfoFrame()
-	{
+	public void makeInfoFrame() {
 		help.setEnabled(false);
 		JDialog infoFrame = new JDialog(this);
 		infoFrame.setTitle("Pricing Information");
 		infoFrame.setResizable(false);
-		
+
 		JPanel Panel = new JPanel(new BorderLayout());
 		GridLayout gl = new GridLayout(0, 2);
 		JPanel InfoPanel = new JPanel(gl);
-		
-		//VenusFlyTrap Info
+
+		// VenusFlyTrap Info
 		JLabel j1 = new JLabel("VenusFlyTrap : ");
 		j1.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
-		j1.setForeground(Color.GREEN); 
-		InfoPanel.add(j1); 
-		
+		j1.setForeground(Color.GREEN);
+		InfoPanel.add(j1);
+
 		JLabel j2 = new JLabel("150 coins");
 		j2.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
 		j2.setForeground(Color.CYAN);
 		InfoPanel.add(j2);
-		
-		//SunFlower Info
+
+		// SunFlower Info
 		JLabel j3 = new JLabel("SunFlower : ");
 		j3.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
-		j3.setForeground(Color.green); 
-		InfoPanel.add(j3); 
-		
+		j3.setForeground(Color.green);
+		InfoPanel.add(j3);
+
 		JLabel j4 = new JLabel("50 coins");
 		j4.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
 		j4.setForeground(Color.CYAN);
 		InfoPanel.add(j4);
-		
-		//Walnut Info
+
+		// Walnut Info
 		JLabel j5 = new JLabel("Walnut : ");
 		j5.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
-		j5.setForeground(Color.green); 
-		InfoPanel.add(j5); 
-		
+		j5.setForeground(Color.green);
+		InfoPanel.add(j5);
+
 		JLabel j6 = new JLabel("50 coins");
 		j6.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
 		j6.setForeground(Color.CYAN);
 		InfoPanel.add(j6);
-		
-		//Potatoe Info
+
+		// Potatoe Info
 		JLabel j7 = new JLabel("Potatoe : ");
 		j7.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
-		j7.setForeground(Color.green); 
-		InfoPanel.add(j7); 
-		
+		j7.setForeground(Color.green);
+		InfoPanel.add(j7);
+
 		JLabel j8 = new JLabel("25 coins");
 		j8.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
 		j8.setForeground(Color.CYAN);
 		InfoPanel.add(j8);
-		
-		//Potatoe Info
+
+		// Potatoe Info
 		JLabel j9 = new JLabel("Pea Shooter : ");
-	    j9.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
-		j9.setForeground(Color.green); 
-		InfoPanel.add(j9); 
-				
+		j9.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
+		j9.setForeground(Color.green);
+		InfoPanel.add(j9);
+
 		JLabel j10 = new JLabel("100 coins");
 		j10.setFont(new Font("TimesNewRoman", Font.BOLD, 13));
 		j10.setForeground(Color.CYAN);
-	    InfoPanel.add(j10);
-		
+		InfoPanel.add(j10);
+
 		Panel.add(InfoPanel, BorderLayout.CENTER);
 		InfoPanel.setOpaque(true);
 		InfoPanel.setBackground(Color.DARK_GRAY);
-		
+
 		infoFrame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent arg0) {
 				help.setEnabled(true);
 			}
 		});
-		
-		infoFrame.add(Panel); //adding the buddy panel to the buddy frame
+
+		infoFrame.add(Panel); // adding the buddy panel to the buddy frame
 		infoFrame.pack();
 		infoFrame.setLocation(0, 100);
 		infoFrame.setVisible(true);
 	}
-	
+
 	public void displayStats(GridObject o) {
 		if (o instanceof Plant)
 			displayPlantStats((Plant) o);
 		else
 			displayZombieStats((Zombie) o);
 	}
-	
+
 	private void displayPlantStats(Plant plant) {
 		JDialog dialog = new JDialog();
 		dialog.setTitle("Plant Stats");
-		dialog.setLayout(new GridLayout(0,2));
-		
+		dialog.setLayout(new GridLayout(0, 2));
+
 		dialog.add(new JLabel("Plant Type:"));
 		dialog.add(new JLabel(plant.getObjectTitle()));
 		dialog.add(new JLabel("Health:"));
-		dialog.add(new JLabel(""+plant.getHealth() + "/" + plant.getFullHealth()));
+		dialog.add(new JLabel("" + plant.getHealth() + "/" + plant.getFullHealth()));
 		dialog.add(new JLabel("Strength:"));
-		dialog.add(new JLabel(""+plant.getStrength()));
+		dialog.add(new JLabel("" + plant.getStrength()));
 		dialog.add(new JLabel("Wait Period:"));
-		dialog.add(new JLabel(""+plant.getCurrentTime() + "/" + plant.getFullTime() + " Turns"));
-		
+		dialog.add(new JLabel("" + plant.getCurrentTime() + "/" + plant.getFullTime() + " Turns"));
+
 		dialog.pack();
 		dialog.setLocationRelativeTo(null);
 		dialog.setVisible(true);
 	}
-	
+
 	private void displayZombieStats(Zombie zombie) {
 		JDialog dialog = new JDialog();
 		dialog.setTitle("Zombie Stats");
-		dialog.setLayout(new GridLayout(0,2));
+		dialog.setLayout(new GridLayout(0, 2));
 
 		dialog.add(new JLabel("Zombie Type:"));
 		dialog.add(new JLabel(zombie.getObjectTitle()));
 		dialog.add(new JLabel("Health:"));
-		dialog.add(new JLabel(""+zombie.getHealth()+ "/" + zombie.getFullHealth()));
+		dialog.add(new JLabel("" + zombie.getHealth() + "/" + zombie.getFullHealth()));
 		dialog.add(new JLabel("Strength:"));
-		dialog.add(new JLabel(""+zombie.getStrength()));
-		
+		dialog.add(new JLabel("" + zombie.getStrength()));
+
 		dialog.pack();
 		dialog.setLocationRelativeTo(null);
 		dialog.setVisible(true);
 	}
-	
-	
+
 	/**
 	 * This method gets the buttons
+	 * 
 	 * @return JButtons
 	 */
 	public JButton[][] getButtons() {
@@ -293,28 +310,31 @@ public class View extends JFrame {
 
 	/**
 	 * This method gets the list of plants that are stored.
+	 * 
 	 * @return menuList
 	 */
 	public JList<JPanel> getPlants() {
 		return menuList;
 	}
-	
+
 	/**
 	 * This method gets JLabel that displays the coins.
+	 * 
 	 * @return coins
 	 */
 	public JLabel getCoins() {
 		return coins;
 	}
-	
+
 	/**
 	 * This method gets the menu item help.
+	 * 
 	 * @return help
 	 */
 	public JMenuItem getHelp() {
 		return help;
 	}
-	
+
 	public JButton getUndoTurn() {
 		return undoTurn;
 	}
